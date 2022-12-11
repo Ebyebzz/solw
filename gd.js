e41c3b40baae6bfbd2a173e12db196bd9d257976
Eby <ebymoozhiyankal22@gmail.com>
2022-12-11 08:50:53 +0000

    Create 4 files

---
+++ gd.js
@@ -0,0 +1,12 @@
+var cowsay = require("cowsay");
+
+console.log(
+  cowsay.say({
+    text: "My number one piece of advice is: you should learn how to program.",
+  })
+);
+/*const Quote = require('inspirational-quotes');
+
+console.log(Quote.getQuote()); // returns quote (text and author)
+console.log(Quote.getQuote({ author: false }); // return quote without author
+console.log(Quote.getRandomQuote()); */
